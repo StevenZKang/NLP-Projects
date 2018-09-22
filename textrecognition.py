701cd3b833e31ba4d08e53430acee45df3785d48
Steven Kang <stevenkang84@yahoo.ca>
2018-09-22 14:37:26 -0400

    Noun Chunking Update

--- textrecognition.py
+++ textrecognition.py
@@ -8,14 +8,21 @@ def tag_text(token_text: list) -> list:
     final_list = []
     for sentence in token_text:
         try:
-            word = nltk.word_tokenize(sentence) #Tokenize each word in the sentence
-            tagged = nltk.pos_tag(word) #Tag the individual word depending on part of speech
-            final_list.append(tagged)
+            words = nltk.word_tokenize(sentence) #Tokenize each word in the sentence into a list
+            tagged = nltk.pos_tag(words) #Tag the individual word depending on part of speech
+            final_list.append(tagged) #Append the list of tuples to final list
 
 
         except Exception as e:
             print(str(e))
     return final_list
+
+def chunk_text(tagged_text: tuple):
+    "Takes a list of tagged tuples and returns a noun chunk"
+    chunkGram = r"""Chunk: {<RB.?>*<VB?>*<NNP>+<NN>?}"""
+    chunkParser = nltk.RegexpParser(chunkGram)
+    chunked = chunkParser.parse(tagged_text)
+    return chunked
 
 training_text = state_union.raw("2005-GWBush.txt")
 sample_text = state_union.raw("2006-GWBush.txt")
@@ -27,6 +34,8 @@ tokenized_text = custom_sent_tokenizer.tokenize(sample_text)
 
 tagged_text = tag_text(tokenized_text)
 print(tagged_text)
+chunked_text = chunk_text(tagged_text[0])
+chunked_text.draw()
 
 
 
