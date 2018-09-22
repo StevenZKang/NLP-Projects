b68dc678780ad6085d4c9f23af3d8814b945df3a
Steven Kang <stevenkang84@yahoo.ca>
2018-09-21 22:50:55 -0400

    Tagging Function Update

--- textrecognition.py
+++ textrecognition.py
@@ -3,16 +3,19 @@ import nltk
 from nltk.corpus import state_union
 from nltk.tokenize import PunktSentenceTokenizer
 
-def tag_text(token_text: list):
+def tag_text(token_text: list) -> list:
     "Tags the input text in tuples"
+    final_list = []
     for sentence in token_text:
         try:
-            word = nltk.word_tokenize(sentence)
-            tagged = nltk.pos_tag(word)
-            print(tagged)
+            word = nltk.word_tokenize(sentence) #Tokenize each word in the sentence
+            tagged = nltk.pos_tag(word) #Tag the individual word depending on part of speech
+            final_list.append(tagged)
+
 
         except Exception as e:
             print(str(e))
+    return final_list
 
 training_text = state_union.raw("2005-GWBush.txt")
 sample_text = state_union.raw("2006-GWBush.txt")
@@ -22,7 +25,8 @@ custom_sent_tokenizer = PunktSentenceTokenizer(training_text)
 #Custom Sentence Tokenizing
 tokenized_text = custom_sent_tokenizer.tokenize(sample_text)
 
-tag_text(tokenized_text)
+tagged_text = tag_text(tokenized_text)
+print(tagged_text)
 
 
 
